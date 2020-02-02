7ebc4aca92edb184be390ada98c13e21cb4b88b8
ylp39 <yanglipei33@gmail.com>
2020-02-02 03:40:29 +0000

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,9 +1,9 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="/">Weibo App</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+      <li class="nav-item"><a href="{{ route('help') }}" class="nav-link">帮助</a></li>
       <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
     </ul>
   </div>
