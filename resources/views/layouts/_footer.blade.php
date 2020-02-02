380f983d4153d457150af95e7bccf378ef2fa761
ylp39 <yanglipei33@gmail.com>
2020-02-02 03:36:08 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png"
+       alt="" class="brand-icon">
+  <a href="https://learnku.com/laravel/courses" target="_blank">
+    刻意练习，每日精进
+  </a>
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,24 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title','Weibo App')  - 我的个人微博</title>
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-</head>
-<body>
+  <head>
+    <title>@yield('title','Weibo App')  - 我的个人微博</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
+  <body>
+    @include('layouts._header')
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="/">Weibo App</a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
-      </ul>
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
     </div>
-  </nav>
-
-  <div class="container">
-    @yield('content')
-  </div>
-
-</body>
+  </body>
 </html>
