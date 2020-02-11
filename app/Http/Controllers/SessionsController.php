e1a74d44c2da56d4f5c24dc4ddd7791aca314b20
ylp39 <yanglipei33@gmail.com>
2020-02-11 16:00:50 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -31,9 +31,15 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials, $request->has('remember'))){
             // 该用户存在于数据库，且邮箱和密码相符合
-            session()->flash('success', '欢迎回来！');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
+            if (Auth::user()->activated){
+                session()->flash('success', '欢迎回来！');
+                $fallback = route('users.show', Auth::user());
+                return redirect()->intended($fallback);
+            }else{
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect('/');
+            }
+
         }else{
             // 登录失败后的相关操作
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'index', 'create', 'store']
+            'except' => ['confirmEmail', 'show', 'index', 'create', 'store']
         ]);
         $this->middleware('guest', [
             'only' => ['create']
@@ -48,9 +49,11 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
-        Auth::login($user);
+//        Auth::login($user);
+        $this->sendEmailConfirmationTo($user);
 
-        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+//        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+        session()->flash('success','感谢注册 Weibo 应用！请确认你的邮箱。');
         return redirect()->route('users.show', [$user]);
     }
 
@@ -86,6 +89,32 @@ class UsersController extends Controller
         $user->delete();
         session()->flash('success', '成功删除用户！');
         return back();
+    }
+
+    public function sendEmailConfirmationTo ($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'summer@example.com';
+        $name = 'Summer';
+        $to = $user->email;
+        $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
+        Mail::send($view,$data,function ($message) use ($from, $name, $to, $subject){
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token',$token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
     }
 
 }
