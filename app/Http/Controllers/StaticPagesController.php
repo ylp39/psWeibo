d5e01bb6ba1e3f0e7d0c56e32da1c7799e7bb1d7
ylp39 <yanglipei33@gmail.com>
2020-02-14 13:59:57 +0000

    发布微博

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class StaticPagesController extends Controller
 {
     public function home(){
-        return view('static_pages/home');
+        return view('static_pages/home',compact(Auth::user()));
     }
 
     public function help()
